36433bea5b45be6aacfa196c88960ad105448ee9
Morten Nissen <mail@hjemmesidekongen.dk>
2020-04-30 22:05:09 +0200

    RK-115

--- web/themes/custom/fds_custom_theme/src/javascripts/app.js
+++ web/themes/custom/fds_custom_theme/src/javascripts/app.js
@@ -1,3 +1,16 @@
 jQuery(function ($) {
   'use strict';
+
+  var slider = tns({
+    container: '.field--name-field-os2web-slideshow-image',
+    items: 1,
+    autoplay: true,
+    autoplayHoverPause: true,
+    gutter: 16,
+    responsive: {
+      576: {
+        items: 2,
+      },
+    },
+  });
 });
