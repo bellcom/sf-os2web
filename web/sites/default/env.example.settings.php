bf5ca3b1051687396cf67a7a0f50b2d23000d665
Andriy Iun <andriy.yun@gmail.com>
2020-05-01 11:56:49 +0200

    Added ability to store drupal environment related configuration.

---
+++ web/sites/default/env.example.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * Contains project environment configuration.
+ */
+
+// Set the environment variable. Allowed values: DEV_ENV, STAGE_ENV, PROD_ENV.
+$settings['project_env'] = DEV_ENV;
