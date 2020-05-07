c155d153a30dcd269eb6c18df3c89ca069ba5c4e
Morten Nissen <mail@hjemmesidekongen.dk>
2020-05-07 10:13:24 +0200

    Correct JS tinyslider undefined error

--- web/themes/custom/fds_custom_theme/src/javascripts/app.js
+++ web/themes/custom/fds_custom_theme/src/javascripts/app.js
@@ -1,16 +1,26 @@
 jQuery(function ($) {
   'use strict';
 
-  var slider = tns({
-    container: '.field--name-field-os2web-slideshow-image',
-    items: 1,
-    autoplay: true,
-    autoplayHoverPause: true,
-    gutter: 32,
-    responsive: {
-      576: {
-        items: 2,
-      },
-    },
-  });
 });
+
+// Tiny Slider slideshow img.
+(function() {
+  var selector = '.field--name-field-os2web-slideshow-image';
+
+  if (document.querySelector(selector) !== null) {
+
+    // Run tiny slider.
+    tns({
+      container: selector,
+      items: 1,
+      autoplay: true,
+      autoplayHoverPause: true,
+      gutter: 32,
+      responsive: {
+        576: {
+          items: 2,
+        },
+      },
+    });
+  }
+})();
