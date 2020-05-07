dc8b2f676cc17ec2999be2ec14baa537d413a823
Morten Nissen <mail@hjemmesidekongen.dk>
2020-05-07 11:48:58 +0200

    Megamenu and language selector

--- web/themes/custom/fds_custom_theme/src/javascripts/app.js
+++ web/themes/custom/fds_custom_theme/src/javascripts/app.js
@@ -3,6 +3,25 @@ jQuery(function ($) {
 
 });
 
+// Language selector.
+// Run through all links and truncate Danish to 2 chars. (ex. Da).
+(function() {
+  var links = document.querySelectorAll('.block-language ul a');
+
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    var text = 	link.textContent || link.innerText;
+    var truncatedText = text.substring(0, 2);
+
+    // Inject the content back into the DOM.
+    if (link.textContent) {
+      link.textContent = truncatedText;
+    } else {
+      link.innerText = truncatedText;
+    }
+  }
+})();
+
 // Tiny Slider slideshow img.
 (function() {
   var selector = '.field--name-field-os2web-slideshow-image';
