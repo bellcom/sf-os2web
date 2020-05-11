3d04723901d1e76a29e6516e88d60a8c11a01f74
Morten Nissen <mail@hjemmesidekongen.dk>
2020-05-11 15:59:51 +0200

    RK-128

--- web/themes/custom/fds_custom_theme/src/javascripts/structure-navigation.js
+++ web/themes/custom/fds_custom_theme/src/javascripts/structure-navigation.js
@@ -15,15 +15,33 @@
   }
 })();
 
-// Prevent first level menu items from going to destination.
+// Toggle subnavigation.
 (function() {
+  function handleToggle(event) {
+    event.preventDefault();
+
+    var element = this;
+    var currentListItem = element.parentElement;
+    var listItems = document.querySelectorAll('.region-header__below .menu-level-0 > li.show-subnavigation');
+
+    // Remove "show submenu" class from all list items.
+    for (var i = 0; i < listItems.length; i++) {
+      var listItem = listItems[i];
+
+      if (currentListItem.isSameNode(listItem)) break; // Make toggle possible, so we can click on the element and remove the subnavigation as well.
+
+      listItem.classList.remove('show-subnavigation');
+    }
+
+    // Add "show submenu" to current list item.
+    currentListItem.classList.toggle('show-subnavigation');
+  }
+
   var links = document.querySelectorAll('.region-header__below .menu-level-0 > li > a');
 
   for (var i = 0; i < links.length; i++) {
     var link = links[i];
 
-    link.addEventListener('click', function(event) {
-      event.preventDefault();
-    })
+    link.addEventListener('click', handleToggle);
   }
 })();
