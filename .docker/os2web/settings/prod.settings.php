f22c0a863ba33370411217998a49ddd31a13de2d
Andriy Iun <andriy.yun@gmail.com>
2021-01-08 17:02:29 +0100

    BKDK-265 Added docker-compose stack. Drupal has to be run as separate image. See ./.docker/os2web/Dockerfile.

---
+++ .docker/os2web/settings/prod.settings.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * Local development override configuration feature.
+ *
+ * To activate this feature, copy and rename it such that its path plus
+ * filename is 'sites/default/settings.local.php'. Then, go to the bottom of
+ * 'sites/default/settings.php' and uncomment the commented lines that mention
+ * 'settings.local.php'.
+ *
+ * If you are using a site name in the path, such as 'sites/example.com', copy
+ * this file to 'sites/example.com/settings.local.php', and uncomment the lines
+ * at the bottom of 'sites/example.com/settings.php'.
+ */
+
+$databases['default']['default'] = [
+  'database' => getenv('MYSQL_DATABASE'),
+  'driver' => 'mysql',
+  'host' => getenv('MYSQL_HOSTNAME'),
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'password' => getenv('MYSQL_PASSWORD'),
+  'port' => getenv('MYSQL_PORT'),
+  'prefix' => '',
+  'username' => getenv('MYSQL_USER'),
+];
+
+$settings['hash_salt'] = getenv('DRUPAL_HASH_SALT');
+
+$settings['file_temp_path'] = '/tmp';
+$settings['file_private_path'] = '../private';
+
