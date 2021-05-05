1a9cc8376b07a4cd9dd871c3f9213fd9311cbc10
Stanislav Kutasevits <stan@bellcom.dk>
2021-05-05 16:12:35 +0300

    BKDK-432 disabling related links on migrated content

--- web/modules/custom/ballerup_d7_migration/scripts/migrate.sh
+++ web/modules/custom/ballerup_d7_migration/scripts/migrate.sh
@@ -48,11 +48,13 @@ echo "Migration ballerup_d7_node_indholdside - START"
 drush migrate:import ballerup_d7_node_indholdside
 echo "Migration ballerup_d7_node_indholdside - END"
 
-echo "Execuing custom script [1/3] - Fix publish status"
+echo "Execuing custom script [1/4] - Fix publish status"
 drush scr modules/custom/ballerup_d7_migration/scripts/migrate_fix_publish_status.php
-echo "Execuing custom script [2/3] - Remove inline picutres"
+echo "Execuing custom script [2/4] - Remove inline picutres"
 drush scr modules/custom/ballerup_d7_migration/scripts/remove_inline_pictures.php
-echo "Execuing custom script [3/3] - Remove node header duplicates (content pages using Borger.dk article)"
+echo "Execuing custom script [3/4] - Remove node header duplicates (content pages using Borger.dk article)"
 drush scr modules/custom/ballerup_d7_migration/scripts/BKDK-401-migrate_remove_page_borgerdk_body.php
+echo "Execuing custom script [4/4] - Disale related links block on migrated nodes"
+drush scr modules/custom/ballerup_d7_migration/scripts/BKDK-432-migrate_disable_related_links.php
 
 echo "Migration complete visit URL '/admin/config/system/delete-orphans' to delete the orphaned paragraphs"
