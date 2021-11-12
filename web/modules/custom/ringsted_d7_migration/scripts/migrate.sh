d6b19d1a84cccb001633053838ff38c2a99bf9d6
Stanislav Kutasevits <stan@bellcom.dk>
2021-11-12 13:35:35 +0200

    RINGSTED-168 migrating news

---
+++ web/modules/custom/ringsted_d7_migration/scripts/migrate.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Migration started"
+
+echo "Importing new import configuration"
+drush cim --partial --source=modules/custom/ringsted_d7_migration/config/install -y
+echo "Configuration imported"
+
+echo "Migration ringsted_d7_taxonomy_section - START"
+drush migrate:import ringsted_d7_taxonomy_section
+echo "Migration ringsted_d7_taxonomy_section - END"
+
+echo "Migration ringsted_d7_node_news - START"
+drush migrate:import ringsted_d7_node_news
+echo "Migration ringsted_d7_node_news - END"
+
+echo "Migration finished"
