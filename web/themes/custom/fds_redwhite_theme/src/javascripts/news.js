6170e6b5f46651e122b4770c0502a1227064ae78
juuliabellcom <juuliadevadze@gmail.com>
2021-12-30 14:29:24 +0200

    RINGSTED Collapse News on front page under 768px

---
+++ web/themes/custom/fds_redwhite_theme/src/javascripts/news.js
@@ -0,0 +1,9 @@
+// Proposals form toggle.
+(function($) {
+  "use strict";
+  $(document).ready(function ($) {
+    $('.path-frontpage .field--name-field-os2web-column-1 .news').on('click', function (evt) {
+      $(this).closest('.field--name-field-os2web-column-1').find('.paragraph--type--os2web-section-paragraph').toggleClass('show');
+    });
+  });
+})(jQuery);
