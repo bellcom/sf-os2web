4c8aa92e187df60fc7635876bd7b6c17520e44f5
juuliabellcom <juulia@bellcom.ee>
2022-01-04 15:07:54 +0200

    RINGSTED News block on frontpage

--- web/themes/custom/fds_redwhite_theme/src/javascripts/news.js
+++ web/themes/custom/fds_redwhite_theme/src/javascripts/news.js
@@ -3,6 +3,7 @@
   "use strict";
   $(document).ready(function ($) {
     $('.path-frontpage .field--name-field-os2web-column-1 .news').on('click', function (evt) {
+      $(this).toggleClass('open');
       $(this).closest('.field--name-field-os2web-column-1').find('.paragraph--type--os2web-section-paragraph').toggleClass('show');
     });
   });
